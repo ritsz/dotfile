685ddf4ed925962817ae754c81b5f005550191ba
Ritesh <r.ranjan789@gmail.com>
2019-05-27 05:51:18 -0400

    scan fix for hidden files

--- gen_cscope.sh
+++ gen_cscope.sh
@@ -15,7 +15,7 @@
 
 # No vcxproj files, CVS directories or CVS temp files needed.
 find $LNX_PATH_LIST \
- ! -path "*.vcxproj*" ! -path "*/CVS/*"  ! -path "*_format"  ! -path "*#*" ! -path "*.cpp.*" ! -path "*.h.*" ! -path "*.C.*" > /BROWSE/$LNX_SNAP/cscope.files
+ -not -path "*/\.*" ! -path "*.vcxproj*" ! -path "*/CVS/*"  ! -path "*_format"  ! -path "*#*" ! -path "*.cpp.*" ! -path "*.h.*" ! -path "*.C.*" > /BROWSE/$LNX_SNAP/cscope.files
 
 echo $LNX_PATH_LIST
 
